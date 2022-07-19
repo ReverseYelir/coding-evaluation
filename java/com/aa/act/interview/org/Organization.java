cc0bbffadc4f1b76b381eabe646e4a6ceb9119ff
Riley Simpson <rcsimpso@gmail.com>
2022-07-19 12:30:35 -0700

    Implemented the hire method of Organization

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,11 +1,12 @@
 package com.aa.act.interview.org;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public abstract class Organization {
 
 	private Position root;
-	
+	private int numEmployees = 1;
 	public Organization() {
 		root = createOrganization();
 	}
@@ -20,8 +21,46 @@ public abstract class Organization {
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
+		// search for position in the organization
+		Position position = findPosition(this.root, title);
+		// If position was found
+		if(position != null) {
+			Optional<Employee> newEmployee = Optional.of(new Employee(this.numEmployees++, person));
+			position.setEmployee(newEmployee);
+		}
+		// Only returns when desired title does not exist in our organization
 		return Optional.empty();
+	}
+	
+	/*
+	 * Traverses all possible positions to find our desired position
+	 * @param root - The starting position
+	 * @param title - The title of the position we are searching for
+	 */
+	private Position findPosition(Position root, String title) {
+		// If current position is our desired position
+		if(root.getTitle().equals(title)) {
+			return root;
+		} else {
+			// Traverse all the direct reporters of the current position
+			for(Position position : root.getDirectReports()) {
+				// Position matches title
+				if(position.getTitle().equals(title)) {
+					return position;
+				} else {
+					// Recurse to find position
+					Position foundPos = findPosition(position, title);
+					// Desired position was not found in subtree
+					if(foundPos == null) {
+						continue;
+					} else {
+						return foundPos;
+					}
+				}
+			}
+		}
+		// Returns when the desired position was not found in current subtree
+		return null;
 	}
 
 	@Override
